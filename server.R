0808e9e80f2742b34ddbdb3b002cb4a8171a1bae
jisunxkim <jisunxkim@gmail.com>
2019-04-13 17:20:42 -0400

    Compledted project to sumit

--- server.R
+++ server.R
@@ -10,9 +10,67 @@
 
 
 library(shiny)
+library(datasets)
+library(ggplot2)
+
+
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
+    
+        observeEvent(input$doButton, 
+                {       
+                        output$dataSummary <- renderPrint({
+                                summary(mtcars)
+                        })
+                        
+                        n_cluster <- input$NumberClusters
 
-  
+                       if (input$modelType == "km") {
+                               set.seed(100)
+                                km_result <- kmeans(mtcars, centers = n_cluster, iter.max = 100)
+                                
+                                cluster_result <- cbind(mtcars, cluster = km_result$cluster)
+                                cluster_doc <- km_result
+                                
+                                output$plotting <- renderPlot({
+                                        ggplot(data = cluster_result, aes(x = mpg, y = hp, colour = as.factor(cluster))) + 
+                                                geom_point(size = 4, alpha = 0.3) +
+                                                labs(title =" MPG vs HP by Cluster", y ="HP", x ="MPG", colour = "cluster number", caption ="Data Source: R mtcars")
+                                })                                
+                                
+
+                       }
+                        
+                        else if (input$modelType == "ha") {
+                                set.seed(100)
+                                distance <- dist(mtcars, method = "euclidean") # distance matrix
+                                fit <- hclust(distance, method = "ward.D2") 
+                                groups <- cutree(fit, k = n_cluster) # cut tree into 5 clusters
+                                # draw dendogram with red borders around the 5 clusters 
+                                cluster_doc <- list(fit, cluster = groups)                                                            
+                                output$plotting <- renderPlot({
+                                        plot(fit)
+                                        rect.hclust(fit, k = n_cluster, borde = "red")
+                                })
+                                
+
+        
+                        }
+ 
+        
+        
+                        if (input$summaryText == 1) {
+                                output$report <- renderPrint({
+                                        print(cluster_doc)
+                                })
+                        }
+                        else output$report <- renderText({
+                                "Summary is not selected"
+                        })
+                        
+                      
+                }
+        )
+
 })
--- ui.R
+++ ui.R
@@ -14,29 +14,36 @@ library(shinyWidgets)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Machine Learing: regression, seleting a learnig rate alpha"),
+  titlePanel("Machine Learing: Classification"),
   setBackgroundImage(src = "background2.jpg"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
             h3("Model Selection"),
-            radioButtons("modelType", "Select a model", choices = c("k-means" = "km", "Mean-Shift Clustering" = "ms"), selected = "km"),
-            checkboxInput("summaryText", "Summary of the result"),            
+            radioButtons("modelType", label =  "Select a model", choices = c("k-means" = "km", "Hierarchical Agglomerative" = "ha"), selected = "km"),
+            checkboxInput("summaryText", label = "Summary of the result"),            
             h3("Select number of clusters"),
-            sliderInput("NumberClusters", "Input cluster numbers", 2, 6, 3),
-            actionButton("doButton", "Run the model!")
+            sliderInput("NumberClusters", label = "Input cluster numbers", 2, 6, 3),
+            actionButton("doButton", label = "Run the model!")
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
             tabsetPanel(
                     tabPanel("Document",
-                             h3("Data Set: mtcars"),
-                     tags$p("Ready to take the Shiny tutorial? If so")),
-                    tags$em("This is great!"),
-                    tags$a(href = "https://www.google.com/", "click here"),
-                    tabPanel("Prediction Result")
+                             h3("Introduction"),
+                     tags$p("This shiny application classifies the 32 automobiles of 1973 to 74 models. The data source is from 1974 Motro Trend US magaine and available from R datasets. A user can chose either K-means or Hierarchical Agglomerative method for the unsupervised classification."),
+                     tags$br(),
+                     h3("Directions"),
+                     h4("1. pick a method"),
+                     h4("2. select number of class"),
+                     h5("3. explore data summary, cluster result and cluster plot tabs for the results")),
+                    tabPanel("Data Summary", verbatimTextOutput("dataSummary")),
+                    tabPanel("Cluster Result Report",
+                             verbatimTextOutput("report") ),
+                    tabPanel("Cluster Plot",
+                             plotOutput("plotting"))
             )
     )
   )
